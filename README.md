16cd2669b4ed5735ee2bb2cb351285dc3792dcff
tiko <tiko@gmail.com>
2019-07-05 13:22:14 +0400

    update

--- README.md
+++
@@ -1,2 +0,0 @@
-# Rival
-Rival
---
+++ js/script.js
@@ -0,0 +1,20 @@
+$('.carousel-main').owlCarousel({
+    items: 5,
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 1500,
+    margin: 10,
+    nav: true,
+    dots: false,
+    navText: ['<img src="img/lef.png" class="">','<img src="img/left.png" class="">'],
+})
+$('.carousel-main1').owlCarousel({
+    items: 3,
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 1500,
+    margin: 10,
+    nav: true,
+    dots: false,
+    navText: ['<img src="img/lef.png" class="">','<img src="img/left.png" class="">'],
+})
